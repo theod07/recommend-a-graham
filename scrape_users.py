608ef8a15caf4680159fe8e683ab471a205e070c
theod07 <abc@def.com>
2016-03-17 19:48:36 -0700

    ready to run scraper on macmini

--- scrape_users.py
+++ scrape_users.py
@@ -18,7 +18,7 @@ def get_users(filename):
     return set(users)
 
 
-def get_page(user, driver, sleeptime=1, down_scrolls=0):
+def get_page(user, driver, sleeptime=1, down_scrolls=1e6):
     '''
     '''
     url = 'http://instagram.com/' + user
@@ -43,20 +43,29 @@ def get_page(user, driver, sleeptime=1, down_scrolls=0):
 
 if __name__ == '__main__':
     SLEEPTIME = 1
-    users = ['patricknorton']
+    SAVE_DIR = '/Users/wonder/galvanize/project/recommend-a-graham/data'
+    DEBUG = False
+
+
+    if DEBUG:
+        users = ['patricknorton', 'taylorswift']
+    else:
+        users = get_users('../data/most_popular.txt')
 
     profile = webdriver.FirefoxProfile()
     profile.set_preference("browser.download.folderList", 2)
     profile.set_preference("browser.download.manager.showWhenStarting", False)
-    profile.set_preference("browser.download.dir", '/Users/wonder/galvanize/project/recommend-a-graham/data')
+    profile.set_preference("browser.download.dir", SAVE_DIR)
     driver = webdriver.Firefox(firefox_profile=profile)
 
     saveas = ActionChains(driver).key_down(Keys.COMMAND).send_keys('s').key_up(Keys.COMMAND)
 
+    rawPages = os.listdir('data/')
     for user in users:
+        if any("@" + user in s for s in rawPages):
+            continue
         get_page(user, driver, sleeptime=SLEEPTIME)
-
         with open('logs/log_scrape_users.txt', 'ab') as f:
-            f.write('{} saved page for {}'.format(strftime('%Y%m%d.%H:%M:%s'), user))
+            f.write('{} saved page for {}'.format(time.strftime('%Y%m%d.%H:%M:%s'), user))
 
-    # driver.close()
+    driver.close()
