063e8e2f14c7d6edf45cc015246e93fd729b3f6b
theod07 <abc@def.com>
2016-03-29 11:29:42 -0700

    debug

--- code/vgg16_cpu.py
+++ code/vgg16_cpu.py
@@ -128,8 +128,8 @@ class vgg16(object):
         # print 'successfully plotted'
         toc = time.clock()
 
-        print 'img_path: {}'.format(img_path)
-        print 'predict time: {}'.format(toc-tic)
+        print u'img_path: {}'.format(img_path)
+        print u'predict time: {}'.format(toc-tic)
         for n, label in enumerate(top):
             plt.text(250, 70 + n * 20, '{}, {}'.format(n+1, self.CLASSES[label]), fontsize=14)
             print '{}, Guess: {}.'.format(n+1, self.CLASSES[label])
