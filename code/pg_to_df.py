41b019be636c625b64208ab0c51eef4536889b2c
ubuntu linux <ubuntu@linux.com>
2016-04-07 18:05:28 -0700

    push from inspiration to repo

---
+++ code/pg_to_df.py
@@ -0,0 +1,21 @@
+import psycopg2 as pg2
+import pandas.io.sql as psql
+
+try:
+	conn = pg2.connect(dbname='image_clusters')
+except:
+	conn = pg2.connect(dbname='image_clusters', host='/var/run/postgresql/')
+
+USER_GROUPS = ['photographers', 'travel', 'most_popular', 'foodies', 'models', 'cats', 'dogs']
+
+for group in USER_GROUPS:
+	print 'retrieving data for {}'.format(group)
+	with open('../data/{}.txt'.format(group), 'r') as f:
+		lines = f.readlines()
+		lines = [l for l in lines if not l.startswith('#')]
+		usernames = [l.split('\n')[0] for l in lines]
+	df = psql.frame_query(\
+		'''SELECT * FROM fc7 WHERE username IN ({});'''\
+		.format("'"+"', '".join(usernames)+"'"), conn)
+
+	df.to_pickle('./{}.pkl'.format(group))
