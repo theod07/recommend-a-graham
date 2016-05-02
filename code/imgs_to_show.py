0d4f59a21f4a77aa12508b426b34d8992048fa86
theod07 <abc@def.com>
2016-05-01 23:29:20 -0700

    create list of imgs for each user

---
+++ code/imgs_to_show.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+from pg_to_df import CATEGORIES
+
+HTMLS_DIR = '~/Volumes/panther/recommend-a-graham/data'
+
+def get_dirmap():
+	with open('../data/html_map.txt', 'r') as f:
+		lines = f.read().splitlines()
+	dirs = {}
+	for line in lines:
+		kv = line.split('!@#$!@#$!@#$ ')
+		dirs[kv[0]] = kv[1]
+	return dirs
+
+def imgs_to_show(HTMLS_DIR, CATEGORIES):
+	
+	dirmap = get_dirmap()
+
+	for category in CATEGORIES:
+		with open('../data/{}.txt'.format(category), 'r') as f:
+			lines = f.readlines()
+			lines = [l for l in lines if not l.startswith('#')]
+			users = [l.split('\n')[0] for l in lines]
+
+		for user in users:
+			files = os.listdir(HTMLS_DIR + '/{}/{}'.format(category,dirmap[user]))
+			jpgs = [f for f in files if f.endswith('.jpg')]
+			selected = np.random.choice(jpgs, size=10, replace=False)
+
+			with open('../data/imgs_to_show.csv', 'a') as f:
+				for jpg in selected:
+					print jpg
+					f.write('{}, {}\n'.format(user, jpg))
+
+if __name__ == '__main__':
+	imgs_to_show(HTMLS_DIR, CATEGORIES)
