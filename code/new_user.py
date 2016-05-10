1465622cbd828bc3d0bc9cb52d96a66c0027b476
theod07 <abc@def.com>
2016-05-09 22:50:10 -0700

    testing

--- code/new_user.py
+++ code/new_user.py
@@ -110,7 +110,9 @@ def main(vtype='fc7'):
 
 	# print out results of most similar users along with their user_group
 	for sim in most_sim:
+		print 'sim: {} '.format(sim)
 		for k,v in user_group_dictionary.items():
+			print 'k: {} '.format(k)
 			if sim in v:
 				print 'group: {}  user: {} '.format(v,sim)
 
