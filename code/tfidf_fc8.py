58757fda6c8ce717fe6d862b86b737af4311d5cc
theod07 <abc@def.com>
2016-05-21 22:21:45 -0700

    create vector_to_document()

--- code/tfidf_fc8.py
+++ code/tfidf_fc8.py
@@ -16,7 +16,7 @@ shortcodes_csv = map(lambda x: "','".join(x), shortcodes)
 users_vectors = []
 for i, user in enumerate(sample_users):
 	df = pd.read_sql('''SELECT * FROM fc8 WHERE shortcode in ('{}');'''.format(shortcodes_csv[i]), conn)
-	df.fc8 = df.fc8.apply(lambda x: np.fromstring(x[1:-1], sep='\n'))	
+	df.fc8 = df.fc8.apply(lambda x: np.fromstring(x[1:-1], sep='\n'))
 	users_vectors.append(df)
 
 vectorsums = []
@@ -24,16 +24,16 @@ for i, user in enumerate(sample_users):
 	vectorsum = users_vectors[i].fc8.values.sum()
 	vectorsums.append(vectorsum)
 # matrix of documents vs words
-# each entry is a word count 
-word_counts = np.concatenate([vectorsums[0][np.newaxis,:], 
+# each entry is a word count
+word_counts = np.concatenate([vectorsums[0][np.newaxis,:],
 							vectorsums[1][np.newaxis,:],
-							vectorsums[2][np.newaxis,:], 
+							vectorsums[2][np.newaxis,:],
 							vectorsums[3][np.newaxis,:]], axis=0)
 # calculate document frequency for each word
 # results in a 1000-element array of count of documents
 df = np.sum(word_counts > 0, axis=0)
-# normalize word_count matrix to get term frequencies 
-# ie. divide each term frequency by total count of number of words in that document 
+# normalize word_count matrix to get term frequencies
+# ie. divide each term frequency by total count of number of words in that document
 tf_norm = np.sqrt((word_counts ** 2).sum(axis=1))
 tf_norm[tf_norm == 0] = 1 # avoid divide by zero
 tf = word_counts / tf_norm.reshape(4,1)
@@ -44,3 +44,8 @@ tfidf = tf * idf
 tfidf_norm = np.sqrt((tfidf ** 2).sum(axis=1))
 tfidf_norm[tfidf_norm == 0] = 1
 tfidf_normed = tfidf / tfidf_norm.reshape(4,1) # we're working with 4 documents (users)
+
+def vector_to_document(vector):
+	list_of_words = [['{}'.format(i)]*j for i,j in enumerate(vector)]
+	list_of_words = [item for sublist in list_of_words for item in sublist]
+	document = ' '.join(list_of_words)
