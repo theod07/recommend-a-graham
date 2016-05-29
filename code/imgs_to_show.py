364d6a41098da0e83b73a02327744981e3107833
theod07 <abc@def.com>
2016-05-28 23:33:54 -0700

    updating copy_imgs_to_dir() to move 100imgs for each user to img directory

--- code/imgs_to_show.py
+++ code/imgs_to_show.py
@@ -5,6 +5,7 @@ import os
 import random
 import shutil
 from pg_to_df import CATEGORIES
+from tfidf_fc8 import make_user_category_dict
 
 # HTMLS_DIR = '~/Volumes/panther/recommend-a-graham/data'
 
@@ -22,14 +23,14 @@ def get_usernames(category):
 		lines = f.readlines()
 		lines = [l for l in lines if not l.startswith('#')]
 		usernames = [l.split('\n')[0] for l in lines]
-	return usernames
+	return username
 
 def imgs_to_show(CATEGORIES):
-	
+
 	dirmap = get_dirmap()
 
 	for category in CATEGORIES:
-		usernames = get_usernames(category)	
+		usernames = get_usernames(category)
 
 		for username in usernames:
 			try:
@@ -55,7 +56,7 @@ def sample_predicted_imgs(category, conn):
 	Generate a list of imgs that have already been predicted for each category.
 	These lists will be used to draw random sample of imgs from to present to new user
 
-	INPUT: category 
+	INPUT: category
 			connection object to postgres database
 	OUTPUT: None. (write to file)
 	'''
@@ -64,10 +65,10 @@ def sample_predicted_imgs(category, conn):
 
 	q = '''
 		SELECT shortcode,
-				username, 
+				username,
 				img_id
-		FROM tracker 
-		WHERE username 
+		FROM tracker
+		WHERE username
 		IN ('{}')
 		AND predicted = 1;
 		'''.format("','".join(usernames))
@@ -85,18 +86,46 @@ def sample_predicted_imgs(category, conn):
 			for i in sample:
 				f.write('{}, {}\n'.format(df.ix[i]['username'],df.ix[i]['img_id']))
 
-def copy_imgs_to_dir(category, dirmap):
-	with open('../data/{}_imgs_to_show.csv'.format(category), 'r') as f:
-		lines = f.readlines()
-		lines = [l for l in lines if not l.startswith('username')]
-	for line in lines:
-		user, img_id = line.split(', ')
-		img_id = img_id.split('\n')[0]
-		try:
-			shutil.copy2('../data/{}/{}/{}'.format(cat, dirmap[user], img_id), 
-						'../imgs/{}/{}_{}'.format(category, user, img_id)
-		except:
-			print 'error {}, {}'.format(user, img_id)
+def copy_imgs_to_dir(dirmap):
+	"""
+	open fc8_pkl_<username>.pkl file
+	extract shortcodes
+	extract img_id for first 100imgs for user
+	copy each img_id from html dir to ./imgs/<user>
+	"""
+	dirs = os.listdir('../fc8_pkls/')
+	user_pkls = [dir for dir in dirs if dir.startswith('fc8_')]
+	tracker_df = pd.read_pickle('./tracker.pkl')
+	user_cat_dict = make_user_category_dict()
+
+	for fname in user_pkls:
+		user = fname.split('fc8_')[-1].split('.pkl')[0]
+		df = pd.read_pickle('../fc8_pkls/{}'.format(fname))
+		shortcodes = df.shortcode.values[:100]
+		# shortcodes_csv = "','".join(shortcodes)
+		img_ids = tracker_df[tracker_df.shortcode.apply(lambda x: x in shortcode)]['img_id']
+		category = user_cat_dict[user]
+
+		for img_id in img_ids:
+			try:
+				shutil.copy2('../data/{}/{}/{}'.format(category, dirmap[user], img_id),
+							'../imgs/{}/{}'.format(user, img_id))
+			except:
+				print 'error: {}, {}'.format(user, img_id)
+
+
+
+	# with open('../data/{}_imgs_to_show.csv'.format(category), 'r') as f:
+	# 	lines = f.readlines()
+	# 	lines = [l for l in lines if not l.startswith('username')]
+	# for line in lines:
+	# 	user, img_id = line.split(', ')
+	# 	img_id = img_id.split('\n')[0]
+	# 	try:
+	# 		shutil.copy2('../data/{}/{}/{}'.format(cat, dirmap[user], img_id),
+	# 					'../imgs/{}/{}_{}'.format(category, user, img_id)
+	# 	except:
+	# 		print 'error {}, {}'.format(user, img_id)
 
 
 if __name__ == '__main__':
