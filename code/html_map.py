2285559a0f8d3afefef8a58fee92600ebe8c9147
theod07 <abc@def.com>
2016-06-16 10:54:15 -0700

    add comments

--- code/html_map.py
+++ code/html_map.py
@@ -3,6 +3,10 @@ import os
 USER_GROUPS = ['cats', 'dogs', 'foodies', 'models', 'most_popular', 'photographers', 'travel']
 
 def get_unique_users():
+	"""
+	the original list of usernames for each group may have overlaps.
+	determine the set of unique usernames across all groups.
+	"""
 	# store usernames in a set for fast look-up and handle duplicates
 	users = set()
 	for group in USER_GROUPS:
@@ -28,32 +32,53 @@ def get_unique_users():
 # by inserting # to start of line
 
 def make_html_map():
+	"""
+	foldername/directory for each user is not the same as their username
+	each directory is a customized name, determined by user.
+	use this function to generate a dictionary that holds the string 
+	directory name for users
+	"""
 	dirs = {}
 	for group in USER_GROUPS:
+		# filter out irrelevant directories
 		ds = [d for d in os.listdir('../data/{}'.format(group)) if d.endswith('files')]
+		# read in usernames from file
 		with open('../data/{}.txt'.format(group), 'r') as f:
 			lines = f.readlines()
 			lines = [l for l in lines if not l.startswith('#')]
 			usernames = [l.split('\n')[0] for l in lines]
+		# scan through directories for the string that contains a given username	
 		for username in usernames:
 			for d in ds:
 				if username in d:
+					# add the winning (username,directoryname) pair to dictionary
 					dirs[username] = d
+		# status update to terminal
 		print 'completed group {}'.format(group)
+	# how many items are in the resulting dictionary?
 	print 'len(dirs): {}'.format(len(dirs))
-
+	# write the results to file
 	with open('../data/html_map.txt', 'ab') as f:
 		for k in dirs:
+			# some directories have stupid encoding...
+			# make up our own "encoding" as a delimiter
 			f.write('{}!@#$!@#$!@#$ {}\n'.format(k, dirs[k]))
 	return
 
 def user_group_dictionary():
+	"""
+	create a dictionary that holds all usernames for a given 
+	user category
+	"""
 	d = {}
+	# scan through each group
 	for group in USER_GROUPS:
+		# read usernames from file
 		with open('../data/{}.txt'.format(group), 'r') as f:
 			lines = f.readlines()
 			lines = [l for l in lines if not l.startswith('#')]
 			usernames = set([l.split('\n')[0] for l in lines])
+		# assign to dictionary
 		d[group] = usernames
 	return d
 
