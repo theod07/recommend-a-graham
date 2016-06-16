f385b79447b672ac9c1e6f7ccc8e1275ad468e4c
theod07 <abc@def.com>
2016-06-16 10:19:57 -0700

    cleaning

--- code/html_map.py
+++ code/html_map.py
@@ -3,22 +3,29 @@ import os
 USER_GROUPS = ['cats', 'dogs', 'foodies', 'models', 'most_popular', 'photographers', 'travel']
 
 def get_unique_users():
+	# store usernames in a set for fast look-up and handle duplicates
 	users = set()
 	for group in USER_GROUPS:
+		# read in the list of users from file
 		with open('../data/{}.txt'.format(group), 'r') as f:
 			lines = f.readlines()
 			lines = [l for l in lines if not l.startswith('#')]
 			usernames = [l.split('\n')[0] for l in lines]
+		# how many usernames are in this group?
 		print 'len({}): {}'.format(group, len(usernames)) 
 		for x in usernames:
+			# print out duplicates to terminal 
 			if x in users:
 				print '{} from group {} already in users'.format(x, group)
+			# add unique entries to set
 			else:
 				users.add(x)
+	# total unique usernames
 	print 'len(users): {}'.format(len(users))
 	return users
 
-# go through and comment out the users that are repetitive from the .txt files
+# mannually go through and comment out the users that are repetitive from the .txt files
+# by inserting # to start of line
 
 def make_html_map():
 	dirs = {}
