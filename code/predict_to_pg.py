b5610ad33d3b7d5ca82be2922084d164bb3ace57
theod07 <abc@def.com>
2016-06-16 19:39:14 -0700

    clean up

--- code/predict_to_pg.py
+++ code/predict_to_pg.py
@@ -1,12 +1,19 @@
-import os
+from database import insert_softmax, insert_fc8, insert_fc7
+from vgg16_cpu import vgg16
 import cPickle as pickle
 import psycopg2 as pg2
-from database import insert_softmax, insert_fc8, insert_fc7
 import numpy as np
-from vgg16_cpu import vgg16
+import os
 np.set_printoptions(threshold=np.nan)
 
 def get_shorts_imgs(username):
+	"""
+	generate a query string for to return information from table 
+	'tracker' for a given username
+	
+	INPUT: 	username, string
+	OUTPUT: query string for postgres database
+	"""
 	q = '''SELECT shortcode, img_id 
 			FROM tracker 
 			WHERE username='{}' AND predicted=0
@@ -14,6 +21,13 @@ def get_shorts_imgs(username):
 	return q.format(username)
 
 def update_predicted(shorts_imgs):
+	"""
+	generate a query string to update table 'tracker' after images
+	have been calculated
+
+	INPUT:  shorts_imgs, string of shortcodes and images
+	OUTPUT: query string for postgres database
+	"""
 	q = '''UPDATE tracker AS t 
 			SET predicted=1 
 			FROM (values {:}) AS done(shortcode, img_id) 
@@ -21,6 +35,12 @@ def update_predicted(shorts_imgs):
 	return q.format(', '.join(map(str, shorts_imgs)))
 	
 def get_dirmap():
+	"""
+	load dictionary from file
+
+	INPUT:  None
+	OUTPUT: dictionary (username:directoryname)
+	"""
 	with open('../data/html_map.txt', 'r') as f:
 		lines = f.read().splitlines()
 	dirs = {}
@@ -29,8 +49,13 @@ def get_dirmap():
 		dirs[kv[0]] = kv[1]
 	return dirs
 
-if __name__ == '__main__':
-	
+def main():
+	"""
+	continually loop through all users
+	feed images through neural network 
+	compute vectors: softmax, fc8, fc7 via vgg16
+	store vectors in postgres database
+	"""
 	DEBUG = False
 	USER_GROUP = raw_input('enter user_group:')
 
@@ -43,10 +68,8 @@ if __name__ == '__main__':
 		usernames = usernames[:2]
 	else:
 		usernames = usernames
-
 	# load the mapping from username to directoryname
 	dirmap = get_dirmap()
-
 	# connect to postgres
 	try:
 		conn = pg2.connect(dbname='image_clusters')
@@ -54,15 +77,11 @@ if __name__ == '__main__':
 		conn = pg2.connect(dbname='image_clusters', host='/var/run/postgresql/')
 
 	c = conn.cursor()
+	# load pretrained neural network
 	nnet = vgg16()
-	
-	# c.execute(insert_softmax(conn, 'aaaaa', np.random.rand(1000)))
-	# c.execute(insert_fc8(conn, 'aaaaa', np.random.rand(4096)))
-	# c.execute(insert_fc7(conn, 'aaaaa', np.random.rand(4096)))
 
-	# conn.commit()
-
-	for i in range(5000):	
+	for i in range(5000):
+		# continually iterate through usernames
 		for username in usernames:
 			c.execute(get_shorts_imgs(username))
 			shorts_imgs = c.fetchall()
@@ -82,4 +101,10 @@ if __name__ == '__main__':
 						f.write('fail predicting for {} {} {}\n'.format(USER_GROUP, username, img))
 
 			c.execute(update_predicted(shorts_imgs))
-			conn.commit() #need execute in order to update db
+			#need execute conn.commit() in order to update db
+			conn.commit() 
+	
+
+
+if __name__ == '__main__':
+	main()
